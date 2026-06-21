f386b33cd389d43b844db301b85be48d4e85df1f
VaishnaviRW <wadgaonkar7744@gmail.com>
2026-06-21 15:47:16 +0530

    ok

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Ecommerce App'),
+      dir: require('path').join(__dirname, './coverage/Ecommerce App2'),
       subdir: '.',
       reporters: [
         { type: 'html' },
